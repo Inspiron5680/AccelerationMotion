43e8eaeb1cd25b70351c27edf390357db1b7b828
Inspiron5980 <puyu986@fuwamofu.com>
2019-09-25 15:13:59 +0900

    [add][update]選択モード移行処理を実装

---
+++ Scripts/UI/ChangeSelectMode.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class ChangeSelectMode : MonoBehaviour, ILaserSelectReceiver
+{
+    int curretTrajectoryID;
+    float FADE_VALUE = 0.3f;
+    float OPAQUE_VALUE = 1.0f;
+    public bool IsSelectMode { get; private set; }
+
+    public void LaserSelectReceiver()
+    {
+        if (TrajectoryControl.TrajectoryParents.Count == 0)
+        {
+            return;
+        }
+
+        if (IsSelectMode)
+        {
+            changeAlpha(OPAQUE_VALUE);
+        }
+        else
+        {
+            changeAlpha(FADE_VALUE);
+            TrajectoryControl.TrajectoryParents[0].GetComponentsInChildren<MeshRenderer>()
+                .Select(renderer => renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, OPAQUE_VALUE))
+                .ToArray();
+        }
+
+        IsSelectMode = !IsSelectMode;
+    }
+
+    void changeAlpha(float alphaValue)
+    {
+        TrajectoryControl.TrajectoryParents
+            .SelectMany(parent => parent.GetComponentsInChildren<MeshRenderer>())
+            .Select(renderer => renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, alphaValue))
+            .ToArray();
+    }
+}
