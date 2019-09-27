cc3a973b6c5c4e588de58985ceaff1818f124af3
Inspiron5980 <puyu986@fuwamofu.com>
2019-09-27 15:51:32 +0900

    [add][update]ストックした軌跡の全削除機能の実装

---
+++ Scripts/UI/DestroyTrajectoryUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTrajectoryUI : MonoBehaviour, ILaserSelectReceiver
+{
+    public void LaserSelectReceiver()
+    {
+        foreach(GameObject parent in TrajectoryControl.TrajectoryParents)
+        {
+            Destroy(parent);
+        }
+
+        TrajectoryControl.TrajectoryParents.Clear();
+    }
+}
