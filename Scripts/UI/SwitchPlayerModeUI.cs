48d1e6adf7f6e33c7a3789b13046e1987d31cf0a
Inspiron5980 <puyu986@fuwamofu.com>
2019-10-02 13:45:30 +0900

    [add][update]モード切替処理によるUI切替の実装

---
+++ Scripts/UI/SwitchPlayerModeUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class SwitchPlayerModeUI : MonoBehaviour, VRUI
+{
+    enum PlayerMode
+    {
+        Pitching, Observe
+    }
+
+    PlayerMode currentMode;
+
+    [SerializeField] GameObject pitchingUI;
+    [SerializeField] GameObject observeUI;
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        pitchingUI.SetActive(true);
+        observeUI.SetActive(false);
+        currentMode = PlayerMode.Pitching;
+    }
+
+    public void Receiver()
+    {
+        switch (currentMode)
+        {
+            case PlayerMode.Pitching:
+                pitchingUI.SetActive(false);
+                observeUI.SetActive(true);
+                currentMode = PlayerMode.Observe;
+                break;
+            case PlayerMode.Observe:
+                pitchingUI.SetActive(true);
+                observeUI.SetActive(false);
+                currentMode = PlayerMode.Pitching;
+                break;
+        }
+    }
+}
